bec506f607478c10567c0910d1fde5c7678a1f7c
IsraZafar <161151945+IsraZafar@users.noreply.github.com>
2024-05-05 21:24:39 +0500

    Add files via upload

---
+++ Assignment 8.dart
@@ -0,0 +1,11 @@
+//QUESTION NO 4:Create a Dart program that checks if a given string is empty or not using if-else statements.//
+void main(){
+  String abc=("herry");
+  String dfs=("");
+  if("herry"==""){
+    print ("empty");
+  } 
+  else{
+    print ("not empty");
+  }
+  }
